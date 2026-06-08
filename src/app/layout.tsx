79ab08c971b51d651e6fbed5538127a2c92070d4
Felipe Araujo <felipe.araujo@nocobi.com>
2026-06-08 11:21:00 -0300

    feat: enable vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Montserrat, Open_Sans } from "next/font/google";
 import "./globals.css";
 
@@ -32,7 +33,10 @@ export default function RootLayout({
       lang="pt-BR"
       className={`${montserrat.variable} ${openSans.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
